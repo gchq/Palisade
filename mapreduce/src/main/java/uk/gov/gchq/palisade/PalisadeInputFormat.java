5fe7e5126e1d298f3d50107629590b10cc6102b2
m09526 <m09526@users.noreply.github.com>
2018-07-17 15:33:17 +0100

    input format docced

--- mapreduce/src/main/java/uk/gov/gchq/palisade/PalisadeInputFormat.java
+++ mapreduce/src/main/java/uk/gov/gchq/palisade/PalisadeInputFormat.java
@@ -42,20 +42,59 @@ import java.util.stream.Collector;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
+/**
+ * The main client class for using Palisade data inside of the Hadoop framework. Clients should set this class as the
+ * input format for a MapReduce job inside their client code.
+ *
+ * @param <K> The key type being retrieved from the requested resources
+ * @param <V> The value type for the map task
+ */
 public class PalisadeInputFormat<K, V> extends InputFormat<K, V> {
+    /**
+     * Char-set for serialising data.
+     */
     public static final Charset UTF8 = Charset.forName("UTF-8");
 
+    /**
+     * Hadoop configuration key for storing requests to be processed.
+     */
     public static final String REGISTER_REQUESTS_KEY = "uk.gov.gchq.palisade.mapreduce.registered.requests";
+
+    /**
+     * Hadoop configuration key for setting the client hint for the maximum number of mappers.
+     */
 
     public static final String MAXIMUM_MAP_HINT_KEY = "uk.gov.gchq.palisade.mapreduce.max.map.hint";
 
+    /**
+     * Default number of mappers to use. Hint only. Zero means unlimited.
+     */
     public static final int DEFAULT_MAX_MAP_HINT = 0;
 
+    /**
+     * The Palisade service instance that serves as the central point for gathering data.
+     */
     private static PalisadeService palisadeService;
 
+    /**
+     * {@inheritDoc}
+     * <p>
+     * This method will try to split the resource details returned from the Palisade service across multiple input
+     * splits as guided by the maxmimum mappers hint. However, we can only honour this hint so far, for example, each
+     * separate data request will generate at least one mapper. At the moment we don't try to balance input splits
+     * across multiple data requests. This may be added in the future.
+     *
+     * @throws IllegalStateException if no data requests have been added with {@link PalisadeInputFormat#addDataRequest(JobContext,
+     *                               RegisterDataRequest)} or if no Palisade service has been set
+     * @see PalisadeInputFormat#setMaxMapTasksHint(JobContext, int)
+     */
     @Override
     public List<InputSplit> getSplits(final JobContext context) throws IOException, InterruptedException {
         Objects.requireNonNull(context, "context");
+        //check we have a service set
+        if (palisadeService == null) {
+            throw new IllegalStateException("no Palisade service has been specified");
+        }
         //get the list for this job
         List<RegisterDataRequest> reqs = getDataRequests(context);
         //sanity check
@@ -127,11 +166,26 @@ public class PalisadeInputFormat<K, V> extends InputFormat<K, V> {
         return new ArrayList<>(splits);
     }
 
+    /**
+     * Specify the Palisade service instance to use for requests. This should be set by clients before the job
+     * launches.
+     *
+     * @param service the Palisade service that requests should be sent to for all jobs
+     * @throws NullPointerException if {@code service} is null
+     */
     public static synchronized void setPalisadeService(final PalisadeService service) {
+        //method is syncced in case Hadoop tries to access it from another thread.
         Objects.requireNonNull(service, "service");
         PalisadeInputFormat.palisadeService = service;
     }
 
+    /**
+     * Get the current Palisade service.
+     *
+     * @return the service
+     * @apiNote This method should ALWAYS be used instead of accessing the field directly, even internally in this
+     * class.
+     */
     public static synchronized PalisadeService getPalisadeService() {
         return palisadeService;
     }
@@ -147,7 +201,15 @@ public class PalisadeInputFormat<K, V> extends InputFormat<K, V> {
         return new PalisadeRecordReader();
     }
 
-    public static void addDataRequest(final JobContext context, final RegisterDataRequest request) throws IOException {
+    /**
+     * Adds a data request for the given job. This is the main method by which clients can request data for a MapReduce
+     * job through Palisade. This will be added to the job configuration and used once a job is submitted.
+     *
+     * @param context the job to be configured
+     * @param request the data request to be added
+     * @throws NullPointerException if anything is null
+     */
+    public static void addDataRequest(final JobContext context, final RegisterDataRequest request) {
         Objects.requireNonNull(context, "context");
         Objects.requireNonNull(request, "request");
         //fetch the existing ones
@@ -159,6 +221,13 @@ public class PalisadeInputFormat<K, V> extends InputFormat<K, V> {
         context.getConfiguration().set(REGISTER_REQUESTS_KEY, reqs);
     }
 
+    /**
+     * Get the list of registered data requests for a job.
+     *
+     * @param context the job to get details for
+     * @return the list data requests currently registered for a job
+     * @throws NullPointerException if {@code context} is null
+     */
     public static List<RegisterDataRequest> getDataRequests(final JobContext context) {
         Objects.requireNonNull(context, "context");
         //retrieve the requests added so far
