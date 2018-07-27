a33950d4e0c09964009e489d871ea722c623ba95
p0000001 <p0000001@users.noreply.github.com>
2018-07-27 15:39:50 +0100

    gh-10 Added the docker example to the verify process. Basic test to start up containers, run example, and stop containers

---
+++ cd/verfiy_example.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+key="ExampleObj"
+example_expected_results=(
+     'ExampleObj[property=item1c,visibility=public,timestamp=20]'
+     'ExampleObj[property=item1d,visibility=private,timestamp=20]'
+     'ExampleObj[property=item2c,visibility=public,timestamp=20]'
+     'ExampleObj[property=item2d,visibility=private,timestamp=20]'
+     'ExampleObj[property=item2d,visibility=private,timestamp=20]'
+     'ExampleObj[property=item2c,visibility=public,timestamp=20]'
+   )
+
+validate_example_output() {
+
+   counter=0;
+   for line in $1
+   do
+     if [[ $line = *"$key"* ]]; then
+       if [[ ${example_expected_results[$line]} ]]; then ((counter++)); fi
+     fi
+   done
+
+   if [ "$counter" -eq "${#example_expected_results[@]}" ]; then
+      return 0
+   fi
+   return 1
+}
--- cd/verify.sh
+++ cd/verify.sh
@@ -1,12 +1,33 @@
 #!/usr/bin/env bash
 
+. ./cd/verfiy_example.sh --source-only
+
 set -e
+
+result=0
 
 if [ "$TRAVIS_PULL_REQUEST" != 'false' ]; then
     echo "Running verify script: mvn -q verify -P analyze -B"
     mvn -q verify -P analyze -B
     echo "Running verify script: mvn -q verify -P test -B"
     mvn -q verify -P test -B
+    echo "Starting the multi-jvm-example containerised"
+    ./example/multi-jvm-example/scripts/dockerComposeUp.sh
+    #Sleep to ensure all containers are up
+    sleep 10
+    echo "Running the example application"
+    OUTPUT=`./example/multi-jvm-example/scripts/run.sh`
+    validate_example_output "$OUTPUT"
+    result=$?
+    echo "Stopping the multi-jvm-example containers"
+    ./example/multi-jvm-example/scripts/dockerComposeDown.sh
     echo "Compiling javadoc"
     mvn -q javadoc:aggregate -P quick
+    
 fi
+
+exit $result
+
+
+
+
