60db1c3d560cfa5b400c830bb8ca1b70c26092c8
m09526 <m09526@users.noreply.github.com>
2018-08-02 11:52:40 +0100

    Basic hadoop set up working

--- example/mapreduce-example/mapreduce-example-runner/src/main/java/uk/gov/gchq/palisade/example/MapReduceExample.java
+++ example/mapreduce-example/mapreduce-example-runner/src/main/java/uk/gov/gchq/palisade/example/MapReduceExample.java
@@ -15,21 +15,45 @@
  */
 package uk.gov.gchq.palisade.example;
 
+import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.LocatedFileStatus;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.fs.RemoteIterator;
 import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import uk.gov.gchq.palisade.example.client.ExampleMapReduceClient;
 import uk.gov.gchq.palisade.service.PalisadeService;
 
+import java.io.File;
+import java.util.Iterator;
+
 public class MapReduceExample extends Configured implements Tool {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(MapReduceExample.class);
+
     public static void main(String[] args) throws Exception {
-        new MapReduceExample().run(args);
+        Configuration conf = new Configuration();
+        //Set job tracker to local implementation
+        conf.set("mapred.job.tracker", "local");
+        //Set file system to local implementation and set root to current directory
+        conf.set("fs.defaultFS", new File(".").toURI().toURL().toString());
+        ToolRunner.run(conf, new MapReduceExample(), args);
     }
 
     @Override
     public int run(String[] strings) throws Exception {
-        final ExampleMapReduceClient client=new ExampleMapReduceClient();
-        final PalisadeService palisadeService= client.getPalisadeService();
+        final ExampleMapReduceClient client = new ExampleMapReduceClient();
+        final PalisadeService palisadeService = client.getPalisadeService();
+        FileSystem fs=FileSystem.get(getConf());
+        System.out.println(fs.getClass());
+        RemoteIterator<LocatedFileStatus> it=fs.listFiles(new Path("."),false);
+        while (it.hasNext()) {
+            System.out.println(it.next());
+        }
         return 0;
     }
 }
