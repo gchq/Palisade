88095f4ffcc523cb5a2296fe4b46de238cb87422
m09526 <m09526@users.noreply.github.com>
2018-09-26 19:05:06 +0100

    Revert "Added file watching to property service"
    This reverts commit 2acadd4a992bb2a95f107a83495182dfb3965d6d.

--- service-impl/cache-service-impl/properties-backing-store/src/main/java/uk/gov/gchq/palisade/cache/service/impl/PropertiesBackingStore.java
+++ service-impl/cache-service-impl/properties-backing-store/src/main/java/uk/gov/gchq/palisade/cache/service/impl/PropertiesBackingStore.java
@@ -21,12 +21,7 @@ import org.slf4j.LoggerFactory;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.NoSuchFileException;
-import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.nio.file.StandardWatchEventKinds;
-import java.nio.file.WatchEvent;
-import java.nio.file.WatchKey;
-import java.nio.file.WatchService;
 import java.time.Duration;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
@@ -36,10 +31,6 @@ import java.util.Base64;
 import java.util.List;
 import java.util.Optional;
 import java.util.Properties;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ThreadFactory;
-import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.stream.Stream;
 
 import static java.util.Objects.requireNonNull;
@@ -82,20 +73,6 @@ public class PropertiesBackingStore implements BackingStore {
      * The file location of this backing store.
      */
     private final String location;
-
-    /**
-     * The watcher for file system events.
-     */
-    private WatchService watcher;
-    /**
-     * The key for the file being watched.
-     */
-    private WatchKey watched;
-
-    /**
-     * Flag to stop modifications triggering the watcher mechanism.
-     */
-    private AtomicBoolean beingPersisted = new AtomicBoolean(false);
 
     /**
      * Create a new backing store instance.
@@ -147,123 +124,9 @@ public class PropertiesBackingStore implements BackingStore {
     }
 
     /**
-     * Load the properties to the backing file.
-     *
-     * @throws IOException if anything failed during the load
-     */
-    private synchronized void load() throws IOException {
-        Path configPath = Paths.get(location);
-        props.load(Files.newInputStream(configPath));
-        LOGGER.debug("Loaded from {}", location);
-        //set up watch to allow us to detect outside changes to file
-        createWatch(configPath);
-    }
-
-    /**
-     * Sets up a watch on the named file. This is used to watch the property backing store for changes from external
-     * sources.
-     *
-     * @param configPath the path to watch
-     */
-    private synchronized void createWatch(final Path configPath) {
-        requireNonNull(configPath, "configPath");
-        try {
-            //check if watcher already active
-            if (watcher != null) {
-                return;
-            }
-            watcher = configPath.getFileSystem().newWatchService();
-            //register this
-            watched = configPath.getParent().register(watcher, StandardWatchEventKinds.ENTRY_MODIFY, StandardWatchEventKinds.ENTRY_CREATE, StandardWatchEventKinds.ENTRY_DELETE);
-            //set up a thread to watch this
-            final ThreadFactory defaultFactory = Executors.defaultThreadFactory();
-            ThreadFactory daemonise = runnable -> {
-                Thread t = defaultFactory.newThread(runnable);
-                t.setDaemon(true);
-                return t;
-            };
-            ExecutorService singleThread = Executors.newSingleThreadExecutor(daemonise);
-            singleThread.submit(this::watchLoop);
-        } catch (IOException e) {
-            LOGGER.warn("Can't create watch on {}. Can't monitor properties backing file for changes", location, e);
-            watcher = null;
-            watched = null;
-            return;
-        }
-    }
-
-    /**
-     * Implements the main watch loop that looks for property backing file changes and triggers a reload when it
-     * happens.
-     *
-     * @return <code>null</code>
-     * @throws IOException if the watch service cannot be closed in event of an error
-     */
-    private Object watchLoop() throws IOException {
-        try {
-            while (true) {
-                WatchKey key = watcher.take();
-                if (!(key.watchable() instanceof Path)) {
-                    continue;
-                }
-                Path watchedPath = (Path) key.watchable();
-                // process events
-                for (WatchEvent<?> event : key.pollEvents()) {
-                    final Path changed = (Path) event.context();
-                    //recompute complete path
-                    final Path completePath = watchedPath.resolve(changed);
-                    //see if the file we care about changed
-                    if (completePath.equals(Paths.get(location))) {
-                        if (beingPersisted.get()) {
-                            LOGGER.debug("File change notification triggered by property change, ignoring it.");
-                            break;
-                        }
-                        LOGGER.info("Properties backing file changed {}", location);
-                        load();
-                        break;
-                    }
-                }
-
-                //notify event handled
-                boolean valid = key.reset();
-                if (!valid) {
-                    LOGGER.debug("No longer watching {}", location);
-                    break;
-                }
-            }
-        } catch (IOException | InterruptedException e) {
-            LOGGER.error("Error while watching {}", location, e);
-        } finally {
-            closeWatchServiceSafely();
-        }
-        //only returns on an exception
-        return null;
-    }
-
-    /**
-     * Safely closes the watch services catching exceptions where necessary.
-     */
-    private synchronized void closeWatchServiceSafely() {
-        WatchKey localKey = watched;
-        WatchService localService = watcher;
-        if (localKey != null) {
-            localKey.cancel();
-            watched = null;
-        }
-        if (localService != null) {
-            try {
-                localService.close();
-            } catch (IOException ignored) {
-                //do nothing
-            }
-            watcher = null;
-        }
-    }
-
-    /**
      * Performs a purge of expired keys and persists the properties to a file.
      */
-    private synchronized void update() {
+    private void update() {
         doKeyExpiry();
         try {
             persist();
@@ -273,15 +136,23 @@ public class PropertiesBackingStore implements BackingStore {
     }
 
     /**
+     * Load the properties to the backing file.
+     *
+     * @throws IOException if anything failed during the load
+     */
+    private void load() throws IOException {
+        props.load(Files.newInputStream(Paths.get(location)));
+        LOGGER.debug("Loaded from {}", location);
+    }
+
+    /**
      * Saves the properties to the backing file.
      *
      * @throws IOException if anything failed during save
      */
     private void persist() throws IOException {
-        beingPersisted.set(true);
         props.store(Files.newOutputStream(Paths.get(location)), "Palisade cache service store.");
         LOGGER.debug("Persisted to {}", location);
-        beingPersisted.set(false);
     }
 
     /**
@@ -319,7 +190,7 @@ public class PropertiesBackingStore implements BackingStore {
      */
     private void removeKey(final String key) {
         requireNonNull(key, "key");
-        LOGGER.debug("Removing expired key {}", key);
+        LOGGER.debug("Removing key {}", key);
         synchronized (this) {
             props.remove(key);
             props.remove(makeDateKey(key));
