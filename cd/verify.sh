3415f1ac01444a8555223fd1a21950854a9582cf
ac74475 <ac74475@users.noreply.github.com>
2018-10-23 13:42:15 +0100

    fixed the bug

--- cd/verify.sh
+++ cd/verify.sh
@@ -7,16 +7,10 @@ set -e
 result=0
 
 if [ "$TRAVIS_PULL_REQUEST" != 'false' ]; then
-    echo "check 0"
-    ls -la /home/travis/build/gchq/Palisade/example/example-model/target/classes
     echo "Running verify script: mvn -q verify -P analyze -B"
     mvn -q verify -P analyze -B
-    echo "check 1"
-    ls -la /home/travis/build/gchq/Palisade/example/example-model/target/classes
     echo "Running verify script: mvn verify -P test -B"
     mvn -q verify -P test -B
-    echo "check 2"
-    ls -la /home/travis/build/gchq/Palisade/example/example-model/target/classes
     echo "Starting the multi-jvm-example containerised"
     ./example/multi-jvm-example/scripts/dockerComposeUp.sh
     #Sleep to ensure all containers are up
@@ -29,11 +23,7 @@ if [ "$TRAVIS_PULL_REQUEST" != 'false' ]; then
     echo "Stopping the multi-jvm-example containers"
     ./example/multi-jvm-example/scripts/dockerComposeDown.sh
     echo "Compiling javadoc"
-    echo "check 3"
-    ls -la /home/travis/build/gchq/Palisade/example/example-model/target/classes
-    mvn clean install -P quick -B
     mvn javadoc:aggregate -P quick -B
-
 fi
 
 exit $result
