80e936b1deb8d647571e0ccb70e3614ea1320367
ac74475 <ac74475@users.noreply.github.com>
2018-10-24 16:24:14 +0100

    fixed typo

--- example/multi-jvm-example/scripts/runDocker.sh
+++ example/multi-jvm-example/scripts/runDocker.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-java -cp example/multi-jvm-example/multi-jvm-example-docker-runner/target/multi-jvm-example-docker-runner-*-shaded.jar uk.gov.gchq.palisade.example.MultiJvmDockerExample
+java -cp example/multi-jvm-example/multi-jvm-example-docker-runner/target/multi-jvm-example-docker-runner-*-shaded.jar uk.gov.gchq.palisade.example.MultiJVMDockerExample
