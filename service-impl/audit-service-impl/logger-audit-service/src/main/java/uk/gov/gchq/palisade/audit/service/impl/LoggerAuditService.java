cda3272a6583ebe9e038150d59a49ed6412f32da
m09526 <m09526@users.noreply.github.com>
2018-10-25 11:12:36 +0100

    gh-122

--- service-impl/audit-service-impl/logger-audit-service/src/main/java/uk/gov/gchq/palisade/audit/service/impl/LoggerAuditService.java
+++ service-impl/audit-service-impl/logger-audit-service/src/main/java/uk/gov/gchq/palisade/audit/service/impl/LoggerAuditService.java
@@ -22,16 +22,14 @@ import org.slf4j.LoggerFactory;
 import uk.gov.gchq.palisade.audit.service.AuditService;
 import uk.gov.gchq.palisade.audit.service.request.AuditRequest;
 
+import java.util.concurrent.CompletableFuture;
+
+import static java.util.Objects.requireNonNull;
+
 /**
- * A LoggerAuditService is a simple implementation of an {@link AuditService}
- * that simply constructs a message and logs it using log4j {@link Logger}.
- * <ul>
- * <li>Messages are logged at INFO logging level.</li>
- * <li>Error messages are logged at ERROR logging level.</li>
- * </ul>
- * <p>
- * An example message is:
- * </p>
+ * A LoggerAuditService is a simple implementation of an {@link AuditService} that simply constructs a message and logs
+ * it using log4j {@link Logger}. <ul> <li>Messages are logged at INFO logging level.</li> <li>Error messages are logged
+ * at ERROR logging level.</li> </ul> <p> An example message is: </p>
  * <pre>
  * 'Alice' accessed 'file1' for 'Payroll' and it was processed using 'Age off and visibility filtering'
  * </pre>
@@ -40,7 +38,8 @@ public class LoggerAuditService implements AuditService {
     private static final Logger LOGGER = LoggerFactory.getLogger(LoggerAuditService.class);
 
     @Override
-    public void audit(final AuditRequest request) {
+    public CompletableFuture<Boolean> audit(final AuditRequest request) {
+        requireNonNull(request, "request");
         final String msg = "'" + request.getUser().getUserId().getId()
                 + "' accessed '" + request.getResource().getId()
                 + "' for '" + request.getContext().getJustification()
@@ -51,5 +50,6 @@ public class LoggerAuditService implements AuditService {
         } else {
             LOGGER.info(msg);
         }
+        return CompletableFuture.completedFuture(Boolean.TRUE);
     }
 }
