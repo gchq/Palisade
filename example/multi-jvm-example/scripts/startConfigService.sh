8b01bf8e3b72830fa876555432900e64d74279e5
ac74475 <ac74475@users.noreply.github.com>
2018-11-13 13:14:52 +0000

    gh-158 - Fixed bug in the multi jvm example start service scripts.

--- example/multi-jvm-example/scripts/startConfigService.sh
+++ example/multi-jvm-example/scripts/startConfigService.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-mvn clean install -Pquick -Pmulti-jvm-example-rest-config-service -pl :multi-jvm-example-rest-config-service -Dstandalone-path=config -Dstandalone-port=8085 $@
+mvn clean install -Pmulti-jvm-example-rest-config-service -pl :multi-jvm-example-rest-config-service -Dstandalone-path=config -Dstandalone-port=8085 $@
