2cd0453481c61c2aef833414da877241ae1a7f0e
ac74475 <ac74475@users.noreply.github.com>
2018-11-21 14:12:03 +0000

    gh-102 - Made a start to the HR data generator

---
+++ example/hr-data-generator/src/main/java/uk/gov/gchq/palisade/example/hrdatagenerator/Employee.java
@@ -0,0 +1,147 @@
+/*
+ * Copyright 2018 Crown Copyright
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.gchq.palisade.example.hrdatagenerator;
+
+import org.ajbrown.namemachine.Name;
+import org.ajbrown.namemachine.NameGenerator;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class Employee {
+
+    private int uid;
+    private Name name;
+    private String dateOfBirth;
+    private PhoneNumber[] contactNumbers;
+    private EmergencyContact[] emergencyContacts;
+    private Address address;
+    private BankDetails bankDetails;
+    private String taxCode;
+    private Ethnicity ethnicity;
+    private Manager manager;
+
+    public static Employee generate(final Random random) {
+        Employee employee = new Employee();
+        employee.setUid(generateUID(random));
+        employee.setName(new NameGenerator().generateName());
+        employee.setDateOfBirth(RandomDateOfBirth.generate(random));
+        employee.setContactNumbers(PhoneNumber.generateMany(random));
+        return employee;
+    }
+
+    private static int generateUID(final Random random) {
+        return random.nextInt();
+    }
+
+    public int getUid() {
+        return uid;
+    }
+
+    public void setUid(final int uid) {
+        this.uid = uid;
+    }
+
+    public Name getName() {
+        return name;
+    }
+
+    public void setName(final Name name) {
+        this.name = name;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(final String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public PhoneNumber[] getContactNumbers() {
+        return contactNumbers;
+    }
+
+    public void setContactNumbers(final PhoneNumber[] contactNumbers) {
+        this.contactNumbers = contactNumbers;
+    }
+
+    public EmergencyContact[] getEmergencyContacts() {
+        return emergencyContacts;
+    }
+
+    public void setEmergencyContacts(final EmergencyContact[] emergencyContacts) {
+        this.emergencyContacts = emergencyContacts;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(final Address address) {
+        this.address = address;
+    }
+
+    public BankDetails getBankDetails() {
+        return bankDetails;
+    }
+
+    public void setBankDetails(final BankDetails bankDetails) {
+        this.bankDetails = bankDetails;
+    }
+
+    public String getTaxCode() {
+        return taxCode;
+    }
+
+    public void setTaxCode(final String taxCode) {
+        this.taxCode = taxCode;
+    }
+
+    public Ethnicity getEthnicity() {
+        return ethnicity;
+    }
+
+    public void setEthnicity(final Ethnicity ethnicity) {
+        this.ethnicity = ethnicity;
+    }
+
+    public Manager getManager() {
+        return manager;
+    }
+
+    public void setManager(final Manager manager) {
+        this.manager = manager;
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this)
+                .append("uid", uid)
+                .append("name", name)
+                .append("dateOfBirth", dateOfBirth)
+                .append("contactNumbers", Arrays.toString(contactNumbers))
+                .append("emergencyContacts", emergencyContacts)
+                .append("address", address)
+                .append("bankDetails", bankDetails)
+                .append("taxCode", taxCode)
+                .append("ethnicity", ethnicity)
+                .append("manager", manager)
+                .toString();
+    }
+}
---
+++ example/hr-data-generator/src/main/java/uk/gov/gchq/palisade/example/hrdatagenerator/RandomDateOfBirth.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2018 Crown Copyright
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.gchq.palisade.example.hrdatagenerator;
+
+import java.util.GregorianCalendar;
+import java.util.Random;
+
+public class RandomDateOfBirth {
+
+    private RandomDateOfBirth(){
+    }
+
+    public static String generate(final Random random) {
+        GregorianCalendar gc = new GregorianCalendar();
+        int year = 1800 + random.nextInt(100);
+        gc.set(gc.YEAR, year);
+        int dayOfYear = random.nextInt(gc.getActualMaximum(gc.DAY_OF_YEAR));
+        gc.set(gc.DAY_OF_YEAR, dayOfYear);
+        return gc.get(gc.DAY_OF_MONTH) + "/" + gc.get(gc.MONTH) + "/" + year;
+    }
+}
