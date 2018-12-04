2ec7a2e937f846126b182257912ef41717952cb6
ac74475 <ac74475@users.noreply.github.com>
2018-12-04 13:58:11 +0000

    gh-197 - fixed bug in the building of services

--- example/multi-jvm-example/scripts/buildServices.sh
+++ example/multi-jvm-example/scripts/buildServices.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-mvn install -Pmulti-jvm-example-rest-palisade-service -pl :multi-jvm-example-rest-palisade-service -Dstandalone-path=palisade -Dstandalone-port=8080
-mvn install -Pmulti-jvm-example-rest-policy-service -pl :multi-jvm-example-rest-policy-service -Dstandalone-path=policy -Dstandalone-port=8081
-mvn install -Pmulti-jvm-example-rest-resource-service -pl :multi-jvm-example-rest-resource-service -Dstandalone-path=resource -Dstandalone-port=8082
-mvn install -Pmulti-jvm-example-rest-user-service -pl :multi-jvm-example-rest-user-service -Dstandalone-path=user -Dstandalone-port=8083
-mvn install -Pmulti-jvm-example-rest-data-service -pl :multi-jvm-example-rest-data-service -Dstandalone-path=data -Dstandalone-port=8084
-mvn install -Pmulti-jvm-example-rest-config-service -pl :multi-jvm-example-rest-config-service -Dstandalone-path=config -Dstandalone-port=8085
+mvn clean install -Pmulti-jvm-example-rest-palisade-service -pl :multi-jvm-example-rest-palisade-service -Dstandalone-path=palisade -Dstandalone-port=8080
+mvn clean install -Pmulti-jvm-example-rest-policy-service -pl :multi-jvm-example-rest-policy-service -Dstandalone-path=policy -Dstandalone-port=8081
+mvn clean install -Pmulti-jvm-example-rest-resource-service -pl :multi-jvm-example-rest-resource-service -Dstandalone-path=resource -Dstandalone-port=8082
+mvn clean install -Pmulti-jvm-example-rest-user-service -pl :multi-jvm-example-rest-user-service -Dstandalone-path=user -Dstandalone-port=8083
+mvn clean install -Pmulti-jvm-example-rest-data-service -pl :multi-jvm-example-rest-data-service -Dstandalone-path=data -Dstandalone-port=8084
+mvn clean install -Pmulti-jvm-example-rest-config-service -pl :multi-jvm-example-rest-config-service -Dstandalone-path=config -Dstandalone-port=8085
