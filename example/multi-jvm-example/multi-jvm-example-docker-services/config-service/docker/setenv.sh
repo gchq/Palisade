913bba7f90788dc0cc08195e5cc57154bc0ff179
ac74475 <ac74475@users.noreply.github.com>
2018-12-13 09:38:16 +0000

    gh-211 - Updated the base path for the docker config and data services.

--- example/multi-jvm-example/multi-jvm-example-docker-services/config-service/docker/setenv.sh
+++ example/multi-jvm-example/multi-jvm-example-docker-services/config-service/docker/setenv.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-export JAVA_OPTS="-Dpalisade.rest.basePath=config-service \
+export JAVA_OPTS="-Dpalisade.rest.basePath=config \
                   -Dpalisade.properties.app.title=rest-config-service \
                   -Dpalisade.rest.bootstrap.path=/usr/local/tomcat/webapps/config/WEB-INF/bootstrapConfig.json"
