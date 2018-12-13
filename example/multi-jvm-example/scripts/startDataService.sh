5e51351b753ebe88ada821a4064871ac407c99b5
ac74475 <ac74475@users.noreply.github.com>
2018-12-13 11:59:03 +0000

    Gh 211 base urls (#212)
    * gh-211 - Updated the base path for the docker config and data services.
    
    * gh-211 - Updated the start service scripts to state the base path.

--- example/multi-jvm-example/scripts/startDataService.sh
+++ example/multi-jvm-example/scripts/startDataService.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 
-java -jar example/multi-jvm-example/multi-jvm-example-rest-data-service/target/multi-jvm-example-rest-data-service-*-executable.jar -httpPort=8084 -extractDirectory=.extract/Data -Dpalisade.rest.config.path=example/example-model/src/main/resources/configRest.json
-
+java -jar example/multi-jvm-example/multi-jvm-example-rest-data-service/target/multi-jvm-example-rest-data-service-*-executable.jar \
+                -httpPort=8084 \
+                -extractDirectory=.extract/Data \
+                -Dpalisade.rest.config.path=example/example-model/src/main/resources/configRest.json \
+                -Dpalisade.properties.app.title=rest-data-service \
+                -Dpalisade.rest.basePath=data
