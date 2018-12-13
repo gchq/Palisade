71ab6965b1c42503b76db5d3065259a1159f7f15
ac74475 <ac74475@users.noreply.github.com>
2018-12-13 09:56:24 +0000

    gh-211 - Updated the start service scripts to state the base path.

--- example/multi-jvm-example/scripts/startDataService.sh
+++ example/multi-jvm-example/scripts/startDataService.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-java -jar example/multi-jvm-example/multi-jvm-example-rest-data-service/target/multi-jvm-example-rest-data-service-*-executable.jar
+java -jar example/multi-jvm-example/multi-jvm-example-rest-data-service/target/multi-jvm-example-rest-data-service-*-executable.jar \
                 -httpPort=8084 \
                 -extractDirectory=.extract/Data \
                 -Dpalisade.rest.config.path=example/example-model/src/main/resources/configRest.json \
--- example/multi-jvm-example/scripts/startPolicyService.sh
+++ example/multi-jvm-example/scripts/startPolicyService.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 java -jar example/multi-jvm-example/multi-jvm-example-rest-policy-service/target/multi-jvm-example-rest-policy-service-*-executable.jar \
-                -httpPort 8081 \
+                -httpPort=8081 \
                 -extractDirectory=.extract/Policy \
                 -Dpalisade.rest.config.path=example/example-model/src/main/resources/configRest.json \
                 -Dpalisade.rest.basePath=policy \
