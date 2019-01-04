b0d46be248605d2096618e1a997ccb7e97337c1e
m09526 <m09526@users.noreply.github.com>
2019-01-04 14:39:44 +0000

    checkstyle

--- service-impl/cache-service-impl/hashmap-backing-store/src/main/java/uk/gov/gchq/palisade/cache/service/impl/HashMapBackingStore.java
+++ service-impl/cache-service-impl/hashmap-backing-store/src/main/java/uk/gov/gchq/palisade/cache/service/impl/HashMapBackingStore.java
@@ -21,6 +21,7 @@ import org.apache.commons.lang3.builder.HashCodeBuilder;
 import org.apache.commons.lang3.builder.ToStringBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
 import uk.gov.gchq.palisade.Util;
 
 import java.time.Duration;
--- service-impl/cache-service-impl/properties-backing-store/src/main/java/uk/gov/gchq/palisade/cache/service/impl/PropertiesBackingStore.java
+++ service-impl/cache-service-impl/properties-backing-store/src/main/java/uk/gov/gchq/palisade/cache/service/impl/PropertiesBackingStore.java
@@ -20,6 +20,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
 import uk.gov.gchq.palisade.Util;
 
 import java.io.IOException;
