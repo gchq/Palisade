611c2bae07fbd5aead3737bfcfed7a1c1cbeb4df
m09526 <m09526@users.noreply.github.com>
2019-01-14 14:08:02 +0000

    change net name:

--- example/multi-jvm-example/scripts/runDocker.sh
+++ example/multi-jvm-example/scripts/runDocker.sh
@@ -2,11 +2,11 @@
 set -e
 
 # Check that the required network is present
-NETWORKS=$(docker network ls -f 'name=palisade-example' --format '{{.Name}}')
-if [[ $(grep -c palisade-example <(echo $NETWORKS)) -lt 1 ]]; then
-    echo "Docker network \"palisade-example\" is not present. Do you need to run the dockerComposeUp.sh script?"
+NETWORKS=$(docker network ls -f 'name=palisadeexample' --format '{{.Name}}')
+if [[ $(grep -c palisadeexample <(echo $NETWORKS)) -lt 1 ]]; then
+    echo "Docker network \"palisadeexample\" is not present. Do you need to run the dockerComposeUp.sh script?"
     exit 1
 fi
 
 docker build -t multi-jvm-example-docker-runner ./example/multi-jvm-example/multi-jvm-example-docker-runner/
-docker run --network=palisade-example_default --rm multi-jvm-example-docker-runner
+docker run --network=palisadeexample_default --rm multi-jvm-example-docker-runner
