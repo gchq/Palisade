1a2eefa933cb455092dd451b715b3f43b5bd6e1e
Tar7575 <43581211+Tar7575@users.noreply.github.com>
2019-02-06 15:10:51 +0000

    gh-183 - addition of Nationality Rule Functionality

--- example/example-model/src/main/java/uk/gov/gchq/palisade/example/rule/NationalityRule.java
+++ example/example-model/src/main/java/uk/gov/gchq/palisade/example/rule/NationalityRule.java
@@ -1,30 +1,41 @@
-/*
- * Copyright 2018 Crown Copyright
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package uk.gov.gchq.palisade.example.rule;
-
 
 import uk.gov.gchq.palisade.Context;
 import uk.gov.gchq.palisade.User;
+import uk.gov.gchq.palisade.UserId;
 import uk.gov.gchq.palisade.example.hrdatagenerator.types.Employee;
+import uk.gov.gchq.palisade.example.hrdatagenerator.types.Manager;
 import uk.gov.gchq.palisade.rule.Rule;
 
+import java.util.Objects;
+import java.util.Set;
+
 public class NationalityRule implements Rule<Employee> {
-    @Override
-    public Employee apply(Employee record, User user, Context context) {
-        return null;
+
+
+    public NationalityRule() {
+    }
+
+
+    private Employee redactRecord( Employee redactedRecord) {
+        redactedRecord.setNationality(null);
+        return redactedRecord;
+    }
+
+    public Employee apply( Employee record, User user, Context context) {
+
+        if (null == record) {
+            return null;
+        }
+
+        Objects.requireNonNull(user);
+        Objects.requireNonNull(context);
+        Set<String> roles = user.getRoles();
+        String purpose = context.getJustification();
+
+        if (roles.contains("HR") & purpose.equals("Staff report")) {
+            return record;
+        }
+        return redactRecord(record);
     }
 }
