914b600c3bcf0a1b78861ca54161542077dadce3
m09526 <m09526@users.noreply.github.com>
2019-02-15 10:59:46 +0000

    first attempt

--- example/multi-jvm-example/scripts/runDocker.sh
+++ example/multi-jvm-example/scripts/runDocker.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-set -e
 
+echo "checking network"
 # Check that the required network is present
 NETWORKS=$(docker network ls -f 'name=palisade_example' --format '{{.Name}}')
 if [[ $(grep -c palisade_example <(echo $NETWORKS)) -lt 1 ]]; then
@@ -8,5 +8,7 @@ if [[ $(grep -c palisade_example <(echo $NETWORKS)) -lt 1 ]]; then
     exit 1
 fi
 
+echo "build"
 docker build -t multi-jvm-example-docker-runner ./example/multi-jvm-example/multi-jvm-example-docker-services/client/
+echo "run docker"
 docker run --network=palisade_example_palisade_network --rm multi-jvm-example-docker-runner
