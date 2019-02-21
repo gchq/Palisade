f81de1e285edacba3880a001913ec0b274434439
ac74475 <ac74475@users.noreply.github.com>
2019-02-21 09:44:40 +0000

    gh-267 - added some checks to make sure the build services script has finished before starting up the services

--- cd/verify.sh
+++ cd/verify.sh
@@ -22,8 +22,32 @@ if [ "$TRAVIS_PULL_REQUEST" != 'false' ]; then
     ./example/multi-jvm-example/scripts/dockerComposeDown.sh
     echo "Starting the multi-jvm-example"
     ./example/multi-jvm-example/scripts/buildServices.sh
+    while [ ! -f example/multi-jvm-example/multi-jvm-example-rest-config-service/target/multi-jvm-example-rest-config-service-*-executable.jar ]; do
+        echo "Cannot find example/multi-jvm-example/multi-jvm-example-rest-config-service/target/multi-jvm-example-rest-config-service-*-executable.jar, sleeping for 1 second to wait for build services to finish"
+        sleep 1
+    done
+    while [ ! -f example/multi-jvm-example/multi-jvm-example-rest-data-service/target/multi-jvm-example-rest-data-service-*-executable.jar ]; do
+        echo "Cannot find example/multi-jvm-example/multi-jvm-example-rest-data-service/target/multi-jvm-example-rest-data-service-*-executable.jar, sleeping for 1 second to wait for build services to finish"
+        sleep 1
+    done
+    while [ ! -f example/multi-jvm-example/multi-jvm-example-rest-palisade-service/target/multi-jvm-example-rest-palisade-service-*-executable.jar ]; do
+        echo "Cannot find example/multi-jvm-example/multi-jvm-example-rest-palisade-service/target/multi-jvm-example-rest-palisade-service-*-executable.jar, sleeping for 1 second to wait for build services to finish"
+        sleep 1
+    done
+    while [ ! -f example/multi-jvm-example/multi-jvm-example-rest-policy-service/target/multi-jvm-example-rest-policy-service-*-executable.jar ]; do
+        echo "Cannot find example/multi-jvm-example/multi-jvm-example-rest-policy-service/target/multi-jvm-example-rest-policy-service-*-executable.jar, sleeping for 1 second to wait for build services to finish"
+        sleep 1
+    done
+    while [ ! -f example/multi-jvm-example/multi-jvm-example-rest-resource-service/target/multi-jvm-example-rest-resource-service-*-executable.jar ]; do
+        echo "Cannot find example/multi-jvm-example/multi-jvm-example-rest-resource-service/target/multi-jvm-example-rest-resource-service-*-executable.jar, sleeping for 1 second to wait for build services to finish"
+        sleep 1
+    done
+    while [ ! -f example/multi-jvm-example/multi-jvm-example-rest-user-service/target/multi-jvm-example-rest-user-service-*-executable.jar ]; do
+        echo "Cannot find example/multi-jvm-example/multi-jvm-example-rest-user-service/target/multi-jvm-example-rest-user-service-*-executable.jar, sleeping for 1 second to wait for build services to finish"
+        sleep 1
+    done
     ./example/multi-jvm-example/scripts/startAllServices.sh
-    # Sleep to allow containers to start
+    # Sleep to allow services to start
     sleep 5s
     echo "Running the example application"
     OUTPUT=`./example/multi-jvm-example/scripts/run.sh | tee /dev/tty`
