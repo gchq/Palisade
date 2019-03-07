305e66970ce66540b7a39b075a33bfa70489234b
ac74475 <ac74475@users.noreply.github.com>
2019-03-07 14:08:44 +0000

    Update service/policy-service/src/main/java/uk/gov/gchq/palisade/policy/service/PolicyService.java
    Co-Authored-By: r34721 <46003579+r34721@users.noreply.github.com>

--- service/policy-service/src/main/java/uk/gov/gchq/palisade/policy/service/PolicyService.java
+++ service/policy-service/src/main/java/uk/gov/gchq/palisade/policy/service/PolicyService.java
@@ -30,7 +30,7 @@ import java.util.concurrent.CompletableFuture;
  * The core API for the policy service.
  * The responsibilities of the policy service is to provide the set of rules
  * (filters or transformations) that need to be applied to each resource that
- * has been requested, based the user and purpose.
+ * has been requested, based the user and context.
  * Note that a resource could be a file, stream, directory or even the system
  * resource (policies added to the system resource would be applied globally).
  */
