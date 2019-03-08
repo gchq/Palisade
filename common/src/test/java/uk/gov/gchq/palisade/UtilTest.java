d61c5661e6ccbf045f21c91e00ceae82540a82ef
ac74475 <ac74475@users.noreply.github.com>
2019-03-08 08:45:48 +0000

    Update common/src/test/java/uk/gov/gchq/palisade/UtilTest.java
    Co-Authored-By: r34721 <46003579+r34721@users.noreply.github.com>

--- common/src/test/java/uk/gov/gchq/palisade/UtilTest.java
+++ common/src/test/java/uk/gov/gchq/palisade/UtilTest.java
@@ -52,7 +52,7 @@ public class UtilTest {
         //given
         final Stream<String> stream = Lists.newArrayList("one", "two").stream();
         final Rules<String> rules = new Rules<String>()
-                .rule("r2", (record, user, purpose) -> record.concat("2ndRule"))
+                .rule("r2", (record, user, context) -> record.concat("2ndRule"))
                 .rule("r3", (record, user, purpose) -> record.concat("3rdRule"));
         //when
         final List<String> result = Util.applyRulesToStream(stream, null, null, rules).collect(Collectors.toList());
