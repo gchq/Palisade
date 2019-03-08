a23f32041b88013594625b34fa2d8cd7ddf5eece
developer6959 <developer6959@gmail.com>
2019-03-08 12:28:44 +0000

    gh-260-Moved AvroSerialiser out of service-impl into common

--- common/src/main/java/uk/gov/gchq/palisade/data/serialise/AvroSerialiser.java
+++ common/src/main/java/uk/gov/gchq/palisade/data/serialise/AvroSerialiser.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package uk.gov.gchq.palisade.data.service.impl.serialiser;
+package uk.gov.gchq.palisade.data.serialise;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
@@ -28,7 +28,6 @@ import org.apache.avro.reflect.ReflectDatumWriter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import uk.gov.gchq.palisade.data.serialise.Serialiser;
 import uk.gov.gchq.palisade.io.Bytes;
 import uk.gov.gchq.palisade.io.BytesOutputStream;
 import uk.gov.gchq.palisade.io.BytesSuppliedInputStream;
