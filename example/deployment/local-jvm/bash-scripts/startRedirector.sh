41e3f3b61f68dffe89ab47382fac39f7fed2ac23
ac74475 <ac74475@users.noreply.github.com>
2019-03-08 09:15:35 +0000

    gh-222 - attempt 2 at fixing scripts

--- example/deployment/local-jvm/bash-scripts/startRedirector.sh
+++ example/deployment/local-jvm/bash-scripts/startRedirector.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 . ./example/deployment/bash-scripts/setScriptPath.sh
-java -cp $EXAMPLESERVICES/multi-jvm-example-rest-redirector/target/multi-jvm-example-rest-redirector-*-shaded.jar \
+java -cp $EXAMPLESERVICES/example-rest-redirector-service/target/example-rest-redirector-service-*-shaded.jar \
                 -Dpalisade.rest.basePath="http://localhost:8080/alter/this/path" \
                 -Dpalisade.rest.config.path="$PWD/example/example-model/src/main/resources/configRest.json" \
         uk.gov.gchq.palisade.redirect.Launcher
