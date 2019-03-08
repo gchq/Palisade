5a02f215000cdafd2bae7689ec294e3355816f64
m09526 <m09526@users.noreply.github.com>
2019-03-08 17:06:59 +0000

    remove check

--- example/multi-jvm-example/scripts/startAllServices.sh
+++ example/multi-jvm-example/scripts/startAllServices.sh
@@ -5,10 +5,7 @@ export DIR=example/multi-jvm-example/scripts
 ${DIR}/stopAllServices.sh > /dev/null 2>&1
 
 # deploy etcd
-${DIR}/startETCD.sh
-if [[ $? -ne 0 ]]; then
-    exit
-fi
+${DIR}/startETCD.sh &
 ${DIR}/waitForHost.sh localhost:2379/health ${DIR}/startConfigService.sh &
 ${DIR}/waitForHost.sh http://localhost:8085/config/v1/status ${DIR}/configureServices.sh
 ${DIR}/startPolicyService.sh &
