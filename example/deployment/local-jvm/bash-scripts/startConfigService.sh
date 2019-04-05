4a341011125fbdde31d4eb3e104edb605a27c5c2
m09526 <m09526@users.noreply.github.com>
2019-04-05 19:46:47 +0100

    Fix local scripts

--- example/deployment/local-jvm/bash-scripts/startConfigService.sh
+++ example/deployment/local-jvm/bash-scripts/startConfigService.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
-. ./example/deployment/bash-scripts/setScriptPath.sh
-java -jar $EXAMPLESERVICES/example-rest-config-service/target/example-rest-config-service-*-executable.jar \
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+. "$DIR/../../bash-scripts/setScriptPath.sh"
+
+java -jar "$EXAMPLESERVICES"/example-rest-config-service/target/example-rest-config-service-*-executable.jar \
                 -httpPort=8085 \
                 -extractDirectory=.extract/Config \
                 -Dpalisade.rest.bootstrap.path=.extract/Config/webapps/config/WEB-INF/classes/bootstrapConfig.json \
