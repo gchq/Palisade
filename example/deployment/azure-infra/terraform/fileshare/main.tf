d4d906b7c922a13845312cab413e713bdad2eb17
Marcus Robinson <marrobi@microsoft.com>
2019-04-05 17:09:18 +0100

    change var name

--- example/deployment/azure-infra/terraform/fileshare/main.tf
+++ example/deployment/azure-infra/terraform/fileshare/main.tf
@@ -1,12 +1,12 @@
 resource "azurerm_resource_group" "fileshare-rg" {
   name     = "${var.resource_group_name}"
-  location = "${var.location}"
+  location = "${var.resource_group_location}"
 }
 
 resource "azurerm_storage_account" "storage" {
   name                     = "${var.storage_account_name}"
   resource_group_name      = "${azurerm_resource_group.fileshare-rg.name}"
-  location                 = "${var.location}"
+  location                 = "${var.resource_group_location}"
   account_tier             = "Standard"
   account_replication_type = "LRS"
 }
