17503128233f76f6e05fde5d65dfec3eff958d23
ac74475 <ac74475@users.noreply.github.com>
2019-04-10 11:48:09 +0100

    modified stuff

--- example/deployment/Azure-AKS/k8s/create_share_secret.sh
+++ example/deployment/Azure-AKS/k8s/create_share_secret.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
-AKS_PERS_RESOURCE_GROUP="tmp-palisade-storage"
-AKS_PERS_STORAGE_ACCOUNT_NAME="palisadestorage"
+AKS_PERS_RESOURCE_GROUP=$1
+AKS_PERS_STORAGE_ACCOUNT_NAME=$2
+AKS_CLUSTER_NAME=$3
 
 STORAGE_KEY=$(az storage account keys list --resource-group $AKS_PERS_RESOURCE_GROUP --account-name $AKS_PERS_STORAGE_ACCOUNT_NAME --query "[0].value" -o tsv)
-
+az aks get-credentials -n $AKS_CLUSTER_NAME -g $AKS_PERS_RESOURCE_GROUP
 kubectl create secret generic data-share-secret --from-literal=azurestorageaccountname=$AKS_PERS_STORAGE_ACCOUNT_NAME --from-literal=azurestorageaccountkey=$STORAGE_KEY
