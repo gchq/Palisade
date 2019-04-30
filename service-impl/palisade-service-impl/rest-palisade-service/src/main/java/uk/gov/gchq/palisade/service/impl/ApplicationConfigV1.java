e32b8e81704593e3fdccf748218050318bc32968
m09526 <m09526@users.noreply.github.com>
2019-04-30 10:27:14 +0100

    gh-350 metrics provider:

--- service-impl/palisade-service-impl/rest-palisade-service/src/main/java/uk/gov/gchq/palisade/service/impl/ApplicationConfigV1.java
+++ service-impl/palisade-service-impl/rest-palisade-service/src/main/java/uk/gov/gchq/palisade/service/impl/ApplicationConfigV1.java
@@ -25,6 +25,9 @@ import uk.gov.gchq.palisade.rest.application.AbstractApplicationConfigV1;
 import uk.gov.gchq.palisade.service.PalisadeMetricProvider;
 import uk.gov.gchq.palisade.service.PalisadeService;
 
+import static java.util.Objects.isNull;
+import static java.util.Objects.nonNull;
+
 public class ApplicationConfigV1 extends AbstractApplicationConfigV1 {
     private static final Logger LOGGER = LoggerFactory.getLogger(ApplicationConfigV1.class);
     private static final Class<?>[] RESOURCES = new Class<?>[]{
@@ -38,12 +41,25 @@ public class ApplicationConfigV1 extends AbstractApplicationConfigV1 {
         ServiceBinder binder = new ServiceBinder(delegate, PalisadeService.class);
         register(binder);
         //optionally register a metric provider
+        PalisadeMetricProvider metrics = null;
         try {
-            PalisadeMetricProvider providerDelegate = RestPalisadeMetricProviderV1.createService(System.getProperty(RestUtil.CONFIG_SERVICE_PATH));
-            register(RestPalisadeMetricProviderV1.class);
-            binder.register(providerDelegate, PalisadeMetricProvider.class);
+            //first we check to see if one has been created in the configuration for us to create
+            metrics = RestPalisadeMetricProviderV1.createService(System.getProperty(RestUtil.CONFIG_SERVICE_PATH));
         } catch (Exception e) {
-            LOGGER.warn("Can't instantiate metrics provider", e);
+            LOGGER.info("No metric provider configured in configuration service");
+        }
+        //second we check to see if the PalisadeService instance above is also a metric provider
+        if (isNull(metrics) && delegate instanceof PalisadeMetricProvider) {
+            metrics = (PalisadeMetricProvider) delegate;
+            LOGGER.info("Using metric provider in PalisadeService instead");
+        }
+        //if we have a valid provider register it
+        if (nonNull(metrics)) {
+            register(RestPalisadeMetricProviderV1.class);
+            binder.register(metrics, PalisadeMetricProvider.class);
+        } else {
+            //else we can't create one
+            LOGGER.warn("Can't instantiate metrics provider");
         }
     }
 }
