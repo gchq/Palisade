3c7d4fa92575d91b1af2177777bca23917dfc865
NigelW <nigel.warburton@digital.ncsc.gov.uk>
2019-05-03 09:53:23 +0100

    gh-335 ongoing tidy up

--- example/deployment/local-k8s/bash-scripts/example.sh
+++ example/deployment/local-k8s/bash-scripts/example.sh
@@ -1,2 +1,8 @@
 #!/usr/bin/env bash
+set -e
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+. "$DIR/../../bash-scripts/setScriptPath.sh"
 kubectl apply -f configure-example/k8sConfigureExample.yaml
+sleep 5
+export EXAMPLE_POD=$(kubectl get pod -l job-name=configure-example -o jsonpath="{.items[0].metadata.name}")
+kubectl logs -f $EXAMPLE_POD
--- example/deployment/local-k8s/bash-scripts/runk8sExample.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -e
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-. "$DIR/../../bash-scripts/setScriptPath.sh"
-"$K8SBASHSCRIPTS/k8sDeployExample.sh"
-sleep 5
-EXAMPLE_POD=$(kubectl get pod -l job-name=configure-example -o jsonpath="{.items[0].metadata.name}")
-kubectl logs -f $EXAMPLE_POD
