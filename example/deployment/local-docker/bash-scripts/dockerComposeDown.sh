9dd6cc43dfa56de2b7e9b7f5e226de4114002771
NigelW <nigel.warburton@digital.ncsc.gov.uk>
2019-05-08 11:34:58 +0100

    gh-335 modify shebang for dockerComposeDown

--- example/deployment/local-docker/bash-scripts/dockerComposeDown.sh
+++ example/deployment/local-docker/bash-scripts/dockerComposeDown.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/bash
 export DIR=$(dirname "$0")
 . "$DIR/../../bash-scripts/setScriptPath.sh"
 docker-compose --no-ansi -f "$EXAMPLE/deployment/local-docker/example-docker-services/docker-compose.yml" -p example down
