9c315a8603842c197996a277f4d45ebca4471555
m09526 <m09526@users.noreply.github.com>
2019-05-13 10:45:20 +0100

    more services done

--- example/deployment/local-jvm/bash-scripts/runFormattedLocalJVMExample.sh
+++ example/deployment/local-jvm/bash-scripts/runFormattedLocalJVMExample.sh
@@ -3,5 +3,7 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 . "$DIR/../../bash-scripts/setScriptPath.sh"
 
-java -cp "$EXAMPLE"/deployment/local-jvm/example-runner/target/example-runner-*-shaded.jar -Dpalisade.rest.config.path=configRest.json uk.gov.gchq.palisade.example.RestExample \
+export PALISADE_REST_CONFIG_PATH="configRest.json"
+
+java -cp "$EXAMPLE"/deployment/local-jvm/example-runner/target/example-runner-*-shaded.jar uk.gov.gchq.palisade.example.RestExample \
     "$EXAMPLE/resources/employee_file0.avro" | $EXAMPLE/deployment/bash-scripts/formatOutput.sh
