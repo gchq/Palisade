19e40ae69b1995cf43e195f60f29dea52875b332
NigelW <nigel.warburton@digital.ncsc.gov.uk>
2019-07-05 11:14:07 +0100

    gh-163 missed setenv.sh for user service

--- example/deployment/local-docker/example-docker-services/user-service/docker/setenv.sh
+++ example/deployment/local-docker/example-docker-services/user-service/docker/setenv.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-PALISADE_REST_CONFIG_PATH="/usr/local/tomcat/webapps/user/WEB-INF/configRest.json"
-
 JAVA_OPTS="-Dpalisade.rest.basePath=user \
-                  -Dpalisade.properties.app.title=rest-user-service"
+                  -Dpalisade.properties.app.title=rest-user-service \
+                  -DPALISADE_REST_CONFIG_PATH=/usr/local/tomcat/webapps/user/WEB-INF/configRest.json"
