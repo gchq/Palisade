70967336a53c9b5247ea4fadf2ee292b862936e6
NigelW <nigel.warburton@digital.ncsc.gov.uk>
2019-07-05 09:01:01 +0100

    gh-163 add support for Java property for config rest path - tomcat8 is not recognising exported variables in setenv.sh

--- example/deployment/local-docker/example-docker-services/resource-service/docker/setenv.sh
+++ example/deployment/local-docker/example-docker-services/resource-service/docker/setenv.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-export PALISADE_REST_CONFIG_PATH="/usr/local/tomcat/webapps/resource/WEB-INF/configRest.json"
-
-export JAVA_OPTS="-Dpalisade.rest.basePath=resource \
-                  -Dpalisade.properties.app.title=rest-resource-service"
+JAVA_OPTS="-Dpalisade.rest.basePath=resource \
+                  -Dpalisade.properties.app.title=rest-resource-service \
+                  -DPALISADE_REST_CONFIG_PATH=/usr/local/tomcat/webapps/resource/WEB-INF/configRest.json"
--- example/deployment/local-docker/example-docker-services/user-service/docker/setenv.sh
+++ example/deployment/local-docker/example-docker-services/user-service/docker/setenv.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-export PALISADE_REST_CONFIG_PATH="/usr/local/tomcat/webapps/user/WEB-INF/configRest.json"
+PALISADE_REST_CONFIG_PATH="/usr/local/tomcat/webapps/user/WEB-INF/configRest.json"
 
-export JAVA_OPTS="-Dpalisade.rest.basePath=user \
+JAVA_OPTS="-Dpalisade.rest.basePath=user \
                   -Dpalisade.properties.app.title=rest-user-service"
