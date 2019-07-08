0b79912d28a396434387648dd5d5ba9bb584646b
NigelW <nigel.warburton@digital.ncsc.gov.uk>
2019-07-08 09:35:23 +0100

    gh-163 add in debug to view secret

--- example/deployment/Azure-AKS/k8s/create_share_secret.sh
+++ example/deployment/Azure-AKS/k8s/create_share_secret.sh
@@ -13,10 +13,14 @@ STORAGE_KEY=$(az storage account keys list --resource-group $STORAGE_RESOURCE_GR
 echo "storage key set to"
 echo $STORAGE_KEY
 
+# You can force delete of the shared secret with
+# nigel@Azure:~$ kubectl delete secret data-share-secret
+
 az aks get-credentials -n $AKS_CLUSTER_NAME -g $AKS_RESOURCE_GROUP
 
 if kubectl get secret data-share-secret; then
     echo "Skipping task as the secret already exists"
 else
     kubectl create secret generic data-share-secret --from-literal=azurestorageaccountname=$AKS_PERS_STORAGE_ACCOUNT_NAME --from-literal=azurestorageaccountkey=$STORAGE_KEY
+    kubectl get secret data-share-secret -o yaml
 fi
