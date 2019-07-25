49ade5294f52392501077d161bbf64d4da6f1592
m09526 <41159661+m09526@users.noreply.github.com>
2019-07-25 15:00:31 +0100

    Update example/deployment/AWS-EMR/terraform-scripts/palisade_cluster.tf
    Co-Authored-By: ac74475 <ac74475@users.noreply.github.com>

--- example/deployment/AWS-EMR/terraform-scripts/palisade_cluster.tf
+++ example/deployment/AWS-EMR/terraform-scripts/palisade_cluster.tf
@@ -37,7 +37,6 @@ resource "aws_emr_cluster" "palisade_cluster" {
     args = ["instance.isMaster=true", "echo running on master node"]
   }
 
-#    name = "bootstrap-master-script"
 #    path = "s3://${aws_s3_bucket_object.emr_bootstrap_script.bucket}/${aws_s3_bucket_object.emr_bootstrap_script.key}"
 #    args = [ "--install-boto --install-bluetalon ${data.terraform_remote_state.gep.bluetalon_private_ip} ${data.terraform_remote_state.gep.bluetalon_private_ip} --debug --efs-dns ${aws_efs_mount_target.gep_emr_efs_mount_az1.dns_name} --efs-mount /test" ]
 #  }
