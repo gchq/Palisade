a57bac9e639377b35c389acd561e3aa85e3f9daf
m09526 <41159661+m09526@users.noreply.github.com>
2019-07-25 15:01:28 +0100

    Update example/deployment/AWS-EMR/terraform-scripts/variables.tf
    Co-Authored-By: ac74475 <ac74475@users.noreply.github.com>

--- example/deployment/AWS-EMR/terraform-scripts/variables.tf
+++ example/deployment/AWS-EMR/terraform-scripts/variables.tf
@@ -10,7 +10,9 @@ variable "aws_region" {
     default = "eu-west-1" 
 } 
 
-variable "aws_access_key" {} 
+variable "aws_access_key" {
+    default = ""
+} 
 
 variable "aws_secret_key" {
     default = ""
