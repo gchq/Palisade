a54587e0fc7bfc337f3da66e0e6a641dd86c81fe
developer01189998819991197253 <ChrisCoDev@gmail.com>
2019-08-08 15:54:09 +0100

    gh-417-using docker etcd store

--- example/deployment/AWS-EC2-S3/bash-scripts/runAWS-EC2-S3Example.sh
+++ example/deployment/AWS-EC2-S3/bash-scripts/runAWS-EC2-S3Example.sh
@@ -9,13 +9,12 @@ then
 fi
 
 #./example/deployment/local-jvm/bash-scripts/buildServices.sh                 # ensure that executables for the services are built before deploying
-
+>/var/tmp/terraformoutput
 # deploy the Palisade service on an ec2 instance
 cd ./example/deployment/AWS-EC2-S3/terraform-scripts/InstanceRunningPalisade
-terraform apply -input=false -auto-approve -var pem_file=$1
+terraform apply -input=false -auto-approve -var pem_file=$1 | tee /var/tmp/terraformoutput
 
-# get the host name and security group from state
-terraform apply -var pem_file=$1 > /var/tmp/terraformoutput
+# get the host name and security group from file
 palisadehost=$(grep palisade_host_private_host_name /var/tmp/terraformoutput | cut -d " " -f3)
 securitygroup=$(grep sgname /var/tmp/terraformoutput | cut -d " " -f3)
 
