b581b93c8699854457ceae38323b977b56cb18ae
pd104923 <54805408+pd104923@users.noreply.github.com>
2019-09-16 14:20:21 +0100

    PAL 165 Update Documentation

--- example/deployment/bash-scripts/formatOutput.sh
+++ example/deployment/bash-scripts/formatOutput.sh
@@ -11,4 +11,11 @@ sed $'s/,address=/\\\naddress=/g'| \
 sed $'s/,bankDetails=/\\\nbankDetails=/g'| \
 sed $'s/,taxCode=/\\\ntaxCode=/g'| \
 sed $'s/,nationality=/\\\nnationality=/g'| \
-sed $'s/,manager=/\\\nmanager=/'
+sed $'s/,manager=/\\\nmanager=/g'| \
+sed $'s/,hireDate=/\\\nhireDate=/g'| \
+sed $'s/,grade=/\\\ngrade=/g'| \
+sed $'s/,department=/\\\ndepartment=/g'| \
+sed $'s/,salaryAmount=/\\\nsalaryAmount=/g'| \
+sed $'s/,salaryBonus=/\\\nsalaryBonus=/g'| \
+sed $'s/,workLocation=/\\\nworkLocation=/g'| \
+sed $'s/,sex=/\\\nsex=/g'
