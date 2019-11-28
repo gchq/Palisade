0ae7502cc8b0a9a66513bf1e141a794b96de1562
dev930018 <dev930018@gmail.com>
2019-11-28 16:01:24 +0000

    Added ignore to Egeria (integration) test (again)

--- service-impl/resource-service-impl/egeria-resource-service/src/test/java/uk/gov/gchq/palisade/resource/service/impl/EgeriaResourceServiceTest.java
+++ service-impl/resource-service-impl/egeria-resource-service/src/test/java/uk/gov/gchq/palisade/resource/service/impl/EgeriaResourceServiceTest.java
@@ -1,6 +1,7 @@
 package uk.gov.gchq.palisade.resource.service.impl;
 
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import uk.gov.gchq.palisade.UserId;
@@ -13,7 +14,7 @@ import java.util.Map;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 
-//@Ignore
+@Ignore
 public class EgeriaResourceServiceTest {
 
     private EgeriaResourceService resourceService;
